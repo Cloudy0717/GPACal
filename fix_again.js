e45f3bd3ee9e9abe8b24c1b2a933b8c0edff3b9f
Cloudy0717 <tucksonquah@gmail.com>
2026-08-13 23:32:08 +0800

    fix: Remove all window prefix references in app.js

---
+++ fix_again.js
@@ -0,0 +1,19 @@
+﻿const fs = require('fs');
+
+function revert(file) {
+    let c = fs.readFileSync(file, 'utf8');
+    c = c.replace(/window\.Storage/g, 'Storage');
+    c = c.replace(/window\.UniversityLoader/g, 'UniversityLoader');
+    c = c.replace(/window\.CourseRow/g, 'CourseRow');
+    c = c.replace(/window\.SemesterRow/g, 'SemesterRow');
+    c = c.replace(/window\.ResultUI/g, 'ResultUI');
+    c = c.replace(/window\.GpaEngine/g, 'GpaEngine');
+    c = c.replace(/window\.CgpaEngine/g, 'CgpaEngine');
+    c = c.replace(/window\.WamRow/g, 'WamRow');
+    c = c.replace(/window\.WamEngine/g, 'WamEngine');
+    
+    fs.writeFileSync(file, c);
+}
+
+revert('src/js/app.js');
+revert('src/js/app-wam.js');
---
+++ revert.js
@@ -0,0 +1,19 @@
+﻿const fs = require('fs');
+
+function revert(file) {
+    let c = fs.readFileSync(file, 'utf8');
+    c = c.replace(/ window\.Storage/g, ' window.Storage');
+    c = c.replace(/ window\.UniversityLoader/g, ' window.UniversityLoader');
+    c = c.replace(/ window\.CourseRow/g, ' window.CourseRow');
+    c = c.replace(/ window\.SemesterRow/g, ' window.SemesterRow');
+    c = c.replace(/ window\.ResultUI/g, ' window.ResultUI');
+    c = c.replace(/ window\.GpaEngine/g, ' window.GpaEngine');
+    c = c.replace(/ window\.CgpaEngine/g, ' window.CgpaEngine');
+    c = c.replace(/ window\.WamRow/g, ' window.WamRow');
+    c = c.replace(/ window\.WamEngine/g, ' window.WamEngine');
+    
+    fs.writeFileSync(file, c);
+}
+
+revert('src/js/app.js');
+revert('src/js/app-wam.js');
